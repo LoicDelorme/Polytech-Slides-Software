d449d180bc55b5e7f8cbb63cc0d0e38760f3060f
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-01-30 17:05:42 +0100

    [update] remove useless form component

--- src/com/delormeloic/generator/view/slidesforms/HeaderForm.java
+++ src/com/delormeloic/generator/view/slidesforms/HeaderForm.java
@@ -4,13 +4,11 @@ import com.delormeloic.generator.model.slides.Header;
 import com.delormeloic.generator.view.converters.FontStringConverter;
 import com.delormeloic.generator.view.helpers.FontsHelper;
 import com.delormeloic.generator.view.helpers.GridPaneHelper;
-import com.delormeloic.generator.view.helpers.SpinnerHelper;
 import com.delormeloic.generator.view.helpers.TextHelper;
 
 import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
@@ -46,21 +44,17 @@ public class HeaderForm implements IFormable
 	{
 		final TextField middleTextTextField = new TextField();
 		final ComboBox<Font> middleTextFontComboBox = new ComboBox<Font>();
-		final Spinner<Integer> middleTextSizeSpinner = new Spinner<Integer>();
 
 		middleTextFontComboBox.getItems().addAll(FontsHelper.getAllAvailableFonts());
 		middleTextFontComboBox.setConverter(new FontStringConverter());
-		middleTextSizeSpinner.setValueFactory(SpinnerHelper.getDefaultTextSizeSpinnerValueFactory());
 
 		middleTextTextField.textProperty().bindBidirectional(this.header.getMiddleTextProperty());
 		middleTextFontComboBox.valueProperty().bindBidirectional(this.header.getMiddleTextFontProperty());
-		middleTextSizeSpinner.getValueFactory().valueProperty().bindBidirectional(this.header.getMiddleTextSizeProperty().asObject());
 
 		final Node[] middleTextField = new Node[] { new Label(TextHelper.getText("headerFormMiddleTextField")), middleTextTextField };
 		final Node[] middleTextFontField = new Node[] { new Label(TextHelper.getText("headerFormMiddleTextFontField")), middleTextFontComboBox };
-		final Node[] middleTextSizeField = new Node[] { new Label(TextHelper.getText("headerFormMiddleTextSizeField")), middleTextSizeSpinner };
 
-		return GridPaneHelper.buildGridPane(new Node[][] { middleTextField, middleTextFontField, middleTextSizeField });
+		return GridPaneHelper.buildGridPane(new Node[][] { middleTextField, middleTextFontField });
 	}
 
 	/**

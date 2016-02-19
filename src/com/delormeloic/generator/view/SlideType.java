82663233446fa34e95daaff07a961e071b5a0d6c
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-02-19 11:47:54 +0100

    [update] renamed enumeration values

--- src/com/delormeloic/generator/view/SlideType.java
+++ src/com/delormeloic/generator/view/SlideType.java
@@ -3,13 +3,13 @@ package com.delormeloic.generator.view;
 import java.lang.reflect.InvocationTargetException;
 
 import com.delormeloic.generator.model.slides.Slide;
+import com.delormeloic.generator.model.slides.SlideWithTitle;
 import com.delormeloic.generator.model.slides.SlideWithTitleWithSpeechWithImage;
 import com.delormeloic.generator.model.slides.SlideWithTitleWithSpeechWithMovie;
-import com.delormeloic.generator.model.slides.SlideWithTitle;
 import com.delormeloic.generator.view.slidesforms.SlideForm;
+import com.delormeloic.generator.view.slidesforms.SlideWithTitleForm;
 import com.delormeloic.generator.view.slidesforms.SlideWithTitleWithSpeechWithImageForm;
 import com.delormeloic.generator.view.slidesforms.SlideWithTitleWithSpeechWithMovieForm;
-import com.delormeloic.generator.view.slidesforms.SlideWithTitleForm;
 import com.delormeloic.utils.logger.Logger;
 
 /**
@@ -26,14 +26,14 @@ public enum SlideType
 	SLIDE_WITH_TITLE("/com/delormeloic/generator/view/resources/images/slide_with_title.png", SlideWithTitle.class, SlideWithTitleForm.class),
 
 	/**
-	 * A slide with a speech with an image.
+	 * A slide with a title with a speech with an image.
 	 */
-	SLIDE_WITH_SPEECH_WITH_IMAGE("/com/delormeloic/generator/view/resources/images/slide_with_speech_with_image.png", SlideWithTitleWithSpeechWithImage.class, SlideWithTitleWithSpeechWithImageForm.class),
+	SLIDE_WITH_TITLE_WITH_SPEECH_WITH_IMAGE("/com/delormeloic/generator/view/resources/images/slide_with_speech_with_image.png", SlideWithTitleWithSpeechWithImage.class, SlideWithTitleWithSpeechWithImageForm.class),
 
 	/**
-	 * A slide with a speech with a movie.
+	 * A slide with a title with a speech with a movie.
 	 */
-	SLIDE_WITH_SPEECH_WITH_MOVIE("/com/delormeloic/generator/view/resources/images/slide_with_speech_with_movie.png", SlideWithTitleWithSpeechWithMovie.class, SlideWithTitleWithSpeechWithMovieForm.class);
+	SLIDE_WITH_TITLE_WITH_SPEECH_WITH_MOVIE("/com/delormeloic/generator/view/resources/images/slide_with_speech_with_movie.png", SlideWithTitleWithSpeechWithMovie.class, SlideWithTitleWithSpeechWithMovieForm.class);
 
 	/**
 	 * The image path.
@@ -86,7 +86,7 @@ public enum SlideType
 	{
 		try
 		{
-			return this.slideFormClass.getConstructor(this.slideClass).newInstance(this.slideClass.getConstructor(String.class).newInstance("defaultName"));
+			return this.slideFormClass.getConstructor(this.slideClass).newInstance(this.slideClass.getConstructor(String.class).newInstance(this.slideClass.getSimpleName()));
 		}
 		catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e)
 		{
